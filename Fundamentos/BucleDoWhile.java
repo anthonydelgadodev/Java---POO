b8c91bcc663ab88c2fc6a2c1dc5303b36a4eca07
Anthony <anthonybdelgado@gmail.com>
2023-07-30 08:16:36 -0500

    #

--- Fundamentos/BucleDoWhile.java
+++ Fundamentos/BucleDoWhile.java
@@ -1,4 +1,36 @@
 /* Bucle Do - while */
 
-/* Sintaxis */
+/* Sintaxis: 
+     Do {
+       Linea1
+       Linea2
+       Linea3
+       Linea4
+     }While (condicion);  
+*/
 
+import javax.swing.*;
+
+public class BucleDoWhile {
+
+    public static void main(String[] args) {
+
+        String genero = "";
+
+        do {
+            genero = JOptionPane.showInputDialog("Introduce tu genero (H/M)");
+
+        } while (genero.equalsIgnoreCase("H") == false && genero.equalsIgnoreCase("M") == false);
+
+        int altura = Integer.parseInt(JOptionPane.showInputDialog("Introduce tu altura en cm"));
+
+        int pesoideal = 0;
+
+        if (genero.equalsIgnoreCase("H")) {
+            pesoideal = altura - 110;
+        } else if (genero.equalsIgnoreCase("M")) {
+            pesoideal = altura - 120;
+        }
+        System.out.println("Tu peso idel es " + pesoideal);
+    }
+}
