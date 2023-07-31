7ddf013321eca3d92e0f2b99ff4dd78914fb4f60
Anthony <anthonybdelgado@gmail.com>
2023-07-31 16:57:23 -0500

    #

---
+++ Fundamentos/Practica/Factorial.java
@@ -0,0 +1,29 @@
+package Practica;
+
+import javax.swing.*;
+
+public class Factorial {
+    
+    public static void main(String[] args) {
+        
+        // Inicialización de la variable para almacenar el resultado del factorial.
+        int resultado = 1;
+        
+        // Solicitar al usuario que ingrese un número utilizando una ventana de diálogo.
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero"));
+        
+        // Inicio del bucle for para calcular el factorial.
+        // La variable 'i' se inicializa con el valor del número ingresado por el usuario.
+        // El bucle se ejecutará mientras 'i' sea mayor que 0.
+        for (int i = numero; i > 0; i--) {
+
+            // Multiplicación acumulada del resultado con el valor actual de 'i'.
+            resultado = resultado * i;
+        }
+        
+        // Fin del bucle for. Se ha calculado el factorial del número ingresado.
+        
+        // Mostrar el resultado del factorial en la consola.
+        System.out.println("El factorial de " + numero + " es " + resultado);
+    }
+}
